a1b7e93eed2cb686706849dbf0395fdbea510f3c
anunezmartinez <adrian@anunezmartinez.com>
2021-01-27 22:38:38 +0100

    aa missing

---
+++ CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float mouseSensivity;
+
+    private Transform parent;
+
+    private void Start()
+    {
+        parent = transform.parent;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        Rotate();
+    }
+
+    private void Rotate() {
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+
+        parent.Rotate(Vector3.up, mouseX);
+    }
+}
